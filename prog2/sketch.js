20faac7f127677c2987be9fa8dee96cfdcd8a9b4
Caleb Langley <clang24@lsu.edu>
2024-02-02 16:39:19 -0600

    Added Prog2
    Prog2 uploaded

---
+++ prog2/sketch.js
@@ -0,0 +1,78 @@
+
+let selectedColor;
+let boxes;
+let mouseClicked = false;
+let x;
+let y;
+
+
+function setup() {
+  createCanvas(800, 400);
+  selectedColor = color('white');
+
+  boxes = [
+    new Box(5, 5, color('red')),
+    new Box(5, 30, color('orange')),
+    new Box(5, 55, color('yellow')),
+    new Box(5, 80, color('green')),
+    new Box(5, 105, color('cyan')),
+    new Box(5, 130, color('blue')),
+    new Box(5, 155, color('magenta')),
+    new Box(5, 180, color('brown')),
+    new Box(5, 205, color('white')),
+    new Box(5 ,230, color('black'))];
+}
+
+function draw() {
+  for(let i=0;i < boxes.length;i++) {
+    boxes[i].draw();
+  }
+
+  fill(selectedColor);
+  circle(width-30,30, 20);
+  
+}
+
+function mousePressed() {
+  let isInBox = false;
+  for(let i=0;i < boxes.length;i++) {
+    if(boxes[i].contains(mouseX,mouseY)) {
+      selectedColor = boxes[i].fill;
+      isInBox = true;
+    }
+  }
+
+  if(!isInBox) {
+    mouseClicked = true;
+  }
+}
+
+function mouseDragged(){
+  if (mouseClicked = true){
+    x += mouseX - pmouseX;
+    y += mouseY - pmouseY;
+  }
+  noStroke();
+  fill(selectedColor);
+  ellipse(pmouseX, pmouseY, 30);
+}
+
+class Box {
+  constructor(x,y, fill) {
+    this.x = x;
+    this.y = y;
+    this.fill = fill;
+  }
+
+  draw() {
+    strokeWeight(3)
+    fill(this.fill);
+    square(this.x,this.y,20);
+  }
+
+  contains(x,y) {
+    let insideX = x >= this.x && x <= this.x+20;
+    let insideY = y >= this.y && y <= this.y+20;
+    return insideX && insideY;
+  }
+}
