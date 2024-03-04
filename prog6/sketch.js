507933b8cce6464cf0164653034a04e21750875b
Caleb Langley <124316916+cj2lang@users.noreply.github.com>
2024-03-03 19:21:18 -0600

    intitial commit, from class

---
+++ prog6/sketch.js
@@ -0,0 +1,53 @@
+let synth1 = new Tone.PolySynth(Tone.Synth);
+let synth2 = new Tone.PolySynth(Tone.DuoSynth);
+let bend = new Tone.PitchShift();
+
+let notes= {
+  'a' : "C4",
+  's' : "D4",
+  'd' : "E4",
+  'f' : "F4",
+  'g' : "G4",
+  'h' : "A4",
+  'j' : "B4",
+  'k' : "C5"
+}
+
+bend.pitch = 0;
+synth1.connect(bend);
+synth2.connect(bend);
+bend.toDestination();
+
+function setup() {
+  createCanvas(400, 400);
+
+  select = createSelect();
+  select.position(150, 180);
+  select.option('Simple Synth');
+  select.option('Duo Synth');
+  select.selected('Simple Synth');
+
+  pitchSlider = createSlider(-12, 12, 0, 0.1);
+  pitchSlider.position (120, 120);
+  pitchSlider.mouseMoved(() => bend.pitch = pitchSlider.value());
+}
+
+function keyPressed (){
+  if(select.selected() == 'Simple Synth'){
+    let playNotes = notes[key];
+    synth1.triggerAttackRelease(playNotes, 0.3);
+  }else if(select.selected() == 'Duo Synth'){
+    let playNotes = notes[key];
+    synth2.triggerAttackRelease(playNotes, 0.3);
+  }
+}
+
+// function keyReleased (){
+//   let playNotes = notes[key];
+//   synth.triggerRelease(playNotes, '+0.03');
+// }
+
+function draw() {
+  background(220);
+  text("Play A-K for Synth", 150, 150);
+}
