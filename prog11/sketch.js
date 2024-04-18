07eb0f67c358d3a87377d0e7ecd86b89761994d2
Caleb Langley <clang24@lsu.edu>
2024-04-17 20:13:14 -0500

    copied prof's code

--- prog11/sketch.js
+++ prog11/sketch.js
@@ -1,45 +1,84 @@
-let connectButton;
 let port;
-let ledState = false;
+let joyX = 0, joyY = 0, sw = 0;
+let connectButton;
+let circleX, circleY;
+let speed = 3;
 
 function setup() {
+  port = createSerial();
   createCanvas(400, 400);
-  port = new p5.WebSerial();
+  circleX = width / 2;
+  circleY = height / 2;
+
   connectButton = createButton("Connect");
   connectButton.mousePressed(connect);
+
+  let usedPorts = usedSerialPorts();
+  if (usedPorts.length > 0) {
+    port.open(usedPorts[0], 57600);
+  }
+  frameRate(20);
 }
 
 function draw() {
   background(220);
-  fill(0);  // Black color for text
-  textSize(20);
-  textAlign(CENTER, CENTER);
-  if (ledState) {
-    text("LED is ON", width / 2, height / 2);
-  } else {
-    text("LED is OFF", width / 2, height / 2);
-  }
-}
 
-function mousePressed() {
-  // Toggle the state of the LED on mouse press
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {  // Check if within canvas
-    ledState = !ledState;
-    if (port.isOpen()) {
-      port.write(ledState ? '1' : '0');  // Send command to Arduino
+
+  // let characters = port.available();
+  // let str = port.read(characters);
+  // let lines = str.split("\n");
+  // let latest = "";
+  // if (lines.length > 0) {
+  //   let lastIndex = lines.length > 1 ? lines.length-2 : lines.length - 1;
+  //   latest = lines[lastIndex];
+  // }
+  let latest = port.readUntil("\n");
+  let values = latest.split(",");
+  if (values.length > 2) {
+    joyX = values[0];
+    joyY = values[1];
+    sw = Number(values[2]);
+
+    if (joyX > 0) {
+      circleX += speed;
+    } else if (joyX < 0) {
+      circleX -= speed;
+    }
+
+    if (joyY > 0) {
+      circleY += speed;
+    } else if (joyY < 0) {
+      circleY -= speed;
     }
   }
+
+  noStroke();
+  fill('purple');
+  rect(0,0,width/2,height);
+  fill('gold');
+  rect(width/2,0,width/2,height);
+
+  if (port.opened() && frameCount % 3 == 0) {
+    let pixel = get(circleX, circleY);
+    console.log(pixel);
+    let message = `${pixel[0]} ${pixel[1]} ${pixel[2]}\n`;
+    port.write(message);
+  }
+
+  stroke(0);
+  if (sw == 1) {
+    fill("blue");
+  }
+  else {
+    fill (255);
+  }
+  circle(circleX, circleY, 5);
 }
 
 function connect() {
   if (!port.opened()) {
-    port.open({ baudRate: 9600 });
-    port.on('open', () => {
-      console.log("Port opened");
-      port.write('1'); // Automatically send '1' to turn on LED
-    });
+    port.open('Arduino', 57600);
   } else {
     port.close();
-    console.log("Port closed");
   }
 }
